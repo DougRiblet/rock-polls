2b01d14d33c6e0cd77920a6298148f659e4e9331
Doug Riblet <smashzen85@gmail.com>
2018-03-22 16:32:56 -0500

    first sass to css build, tweak reducer, react-router nav working

--- client/reducers/root-reducer.jsx
+++ client/reducers/root-reducer.jsx
@@ -5,12 +5,12 @@ import * as types from '../actions/action-types';
 // eslint-disable-next-line no-undef
 const reducer = (state: Object, action: Object): Object => {
   switch (action.type) {
-    case LOGIN_USER:
+    case types.LOGIN_USER:
       return Object.assign({}, state, {
         isAuthenticated: true,
         user: action.user,
       });
-    case LOGOUT_USER:
+    case types.LOGOUT_USER:
       return Object.assign({}, state, {
         isAuthenticated: false,
         user: {},
