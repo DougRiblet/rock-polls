02526373070c0efb943d3146bdbcd724e0d15609
Doug Riblet <smashzen85@gmail.com>
2018-03-23 23:43:25 -0500

    build user model for db, start passport config file

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,22 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
+
+// Borrowing from tutorial at:
+// https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+
+var userSchema = mongoose.Schema({
+  local: {
+    email: String,
+    password: String,
+  }
+});
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
