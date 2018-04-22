30e966a007bb0f14b1dbc58106ac8e03fb1f50a3
Doug Riblet <smashzen85@gmail.com>
2018-04-22 14:37:46 -0500

    connect mongoose

--- server/server.js
+++ server/server.js
@@ -10,27 +10,27 @@ let port = process.env.PORT || 8357;
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// // MONGOOSE 
+// MONGOOSE 
 
-// if(!process.env.MONGODB_URI){
-//   var uri = require( './uri' ).uri;
-// } else {
-//   var uri = process.env.MONGODB_URI;
-// }
+if(!process.env.MONGODB_URI){
+  var uri = require( './uri' ).uri;
+} else {
+  var uri = process.env.MONGODB_URI;
+}
 
-// mongoose.Promise = require('bluebird');
+mongoose.Promise = require('bluebird');
 
-// mongoose.connect(uri)
-//   .then(() => {
-//     console.log('Mongoose connection established');
-//   })
-//   .catch(err => {
-//     console.error('App starting error:', err.stack);
-//     process.exit(1);
-//   });
+mongoose.connect(uri)
+  .then(() => {
+    console.log('Mongoose connection established');
+  })
+  .catch(err => {
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+  });
 
 // // SCHEMA
 
@@ -47,8 +47,6 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
-
-
 
 app.get('/favicon.ico', function (req, res) {
   res.sendStatus(200);
