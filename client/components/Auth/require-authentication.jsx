677b988d86d607f2e638442d6683272477ad70be
Doug Riblet <smashzen85@gmail.com>
2018-05-02 13:59:08 -0500

    tweak to react-router syntax

--- client/components/Auth/require-authentication.jsx
+++ client/components/Auth/require-authentication.jsx
@@ -7,13 +7,13 @@ export default function(ComposedComponent) {
 
     componentWillMount() {
       if (!this.props.authenticated) {
-        this.context.router.push('/');
+        this.context.router.history.push('/');
       }
     }
 
     componentWillUpdate(nextProps) {
       if (!nextProps.authenticated) {
-        this.context.router.push('/');
+        this.context.router.history.push('/');
       }
     }
 
