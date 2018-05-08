71fb22effa5966fd3996f440f4c40192b9d5a764
Doug Riblet <smashzen85@gmail.com>
2018-05-08 18:02:23 -0500

    fix linting errors for server auth folder

--- server/auth/signin.js
+++ server/auth/signin.js
@@ -1,21 +1,20 @@
-require("dotenv").load();
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+require('dotenv').load();
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-let signin = async function(req, res){  
+const signin = async function signin(req, res) {
   try {
-    let user = await User.findOne({username: req.body.username});
-    let compareResults = await user.comparePassword(req.body.password);
-    let { id, username } = user;
+    const user = await User.findOne({ username: req.body.username });
+    const compareResults = await user.comparePassword(req.body.password);
+    const { id, username } = user;
     if (compareResults) {
-      let token = jwt.sign({ id, username }, process.env.JWT_KEY);
+      const token = jwt.sign({ id, username }, process.env.JWT_KEY);
       return res.status(200).json({ id, username, token });
-    } else {
-      return res.status(400).send('Signin rejected');
     }
-  } catch(error) {
-      return res.status(400).send(error);
+    return res.status(400).send('Signin rejected');
+  } catch (error) {
+    return res.status(400).send(error);
   }
-}
+};
 
 module.exports = signin;
--- server/auth/signup.js
+++ server/auth/signup.js
@@ -1,12 +1,12 @@
-require("dotenv").load();
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+require('dotenv').load();
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-const signup = async function(req, res) {
+const signup = async function signup(req, res) {
   try {
-    let newuser = await User.create(req.body);
-    let { username, id } = newuser;
-    let token = jwt.sign({ id, username }, process.env.JWT_KEY);
+    const newuser = await User.create(req.body);
+    const { username, id } = newuser;
+    const token = jwt.sign({ id, username }, process.env.JWT_KEY);
     return res.status(200).json({ username, id, token });
   } catch (error) {
     return res.status(400).send(error);
