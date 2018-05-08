d254b7e7cf1953dea105f48318505bf1bc5f71c2
Doug Riblet <smashzen85@gmail.com>
2018-05-08 15:22:43 -0500

    create login-check middleware

---
+++ server/middleware/login-check.js
@@ -0,0 +1,17 @@
+require("dotenv").load();
+const jwt = require("jsonwebtoken");
+
+const loginCheck = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+      if (decoded && decoded.id === req.body.id) {
+        return next();
+      } else {
+        return res.status(401).send('Not logged in');
+      }
+    });
+  } catch (error) {
+    return res.status(401).send(error);
+  }
+};
