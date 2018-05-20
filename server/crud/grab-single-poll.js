fcd2c9364af790adc57c37d456b2ceb5937b96f8
Doug Riblet <smashzen85@gmail.com>
2018-05-20 16:41:35 -0500

    start building grabSinglePoll handler

---
+++ server/crud/grab-single-poll.js
@@ -0,0 +1,9 @@
+require('dotenv').load();
+const Poll = require('../models/poll');
+const Answer = require('../models/answer');
+
+const grabSinglePoll = async function grabSinglePoll(req, res) {
+
+};
+
+module.exports = grabSinglePoll;
