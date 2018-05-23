9c1c9ea695295cb692e032a8fd540cd59b3df29c
Doug Riblet <smashzen85@gmail.com>
2018-05-23 16:30:20 -0500

    fix linting issues in Single component

--- client/components/Single/single-presentational.jsx
+++ client/components/Single/single-presentational.jsx
@@ -5,9 +5,9 @@ import AnswerItemVote from './answer-item-vote';
 import AltAnswerForm from './alt-answer-form';
 
 type Props = {
-  castVote: (answerId) => mixed,
-  grabSinglePoll: (pollId) => mixed,
-  addAltAnswer: (pollId, answer, user_id) => mixed,
+  castVote: (string) => mixed,
+  grabSinglePoll: (string) => mixed,
+  addAltAnswer: (string, string, string) => mixed,
   authenticated: boolean,
   user_id: string,
   allPolls: Object,
@@ -24,10 +24,10 @@ export default class Single extends React.Component<Props, State> {
     this.state = {
       hasVoted: false,
     };
-  this.displayQuestion = this.displayQuestion.bind(this);
-  this.displayAnswers = this.displayAnswers.bind(this);
-  this.displayAlt = this.displayAlt.bind(this);
-  this.handleVote = this.handleVote.bind(this);
+    this.displayQuestion = this.displayQuestion.bind(this);
+    this.displayAnswers = this.displayAnswers.bind(this);
+    this.displayAlt = this.displayAlt.bind(this);
+    this.handleVote = this.handleVote.bind(this);
   }
 
   componentDidMount() {
@@ -42,29 +42,33 @@ export default class Single extends React.Component<Props, State> {
   }
 
   displayAnswers(pollid) {
-    const answers = this.props.allPolls[pollid].answers;
+    const { answers } = this.props.allPolls[pollid];
     if (answers) {
-      return answers.map(aId => {
+      return answers.map((aId) => {
         const aInfo = this.props.allAnswers[aId];
-        return <AnswerItemVote
+        return (
+          <AnswerItemVote
             key={aId}
             aId={aId}
             aInfo={aInfo}
             handleVote={this.handleVote}
           />
+        );
       });
     }
-    return <li></li>
+    return <li />;
   }
 
   displayAlt(pollid) {
     if (this.props.authenticated) {
-      return <AltAnswerForm
-        handleAlt={this.handleAlt}
-        pollid={pollid}
+      return (
+        <AltAnswerForm
+          handleAlt={this.handleAlt}
+          pollid={pollid}
         />
+      );
     }
-    return <p>Don't like these options? Login or signup to add your own answer.</p>;
+    return <p>Don\'t like these options? Login or signup to add your own answer.</p>;
   }
 
   handleAlt(pollId, answerText) {
@@ -77,7 +81,7 @@ export default class Single extends React.Component<Props, State> {
   }
 
   render() {
-    const pollid = this.props.match.params.pollid;
+    const { pollid } = this.props.match.params;
     return (
       <div id='single'>
         <h2>
