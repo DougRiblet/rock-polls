bb42de2083d4c9d708d3b5c694f9e2be2393f657
Doug Riblet <smashzen85@gmail.com>
2018-05-25 11:33:27 -0500

    rearrange header component layout

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,15 +6,24 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/polls'>Polls</Link></li>
-        <li><Link to='/admin'>Admin</Link></li>
-        <li><Link to='/admin/create'>Create</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/logout'>Logout</Link></li>
-        <li><Link to='/signup'>Signup</Link></li>
-      </ul>
+      <div id='header-full'>
+        <div id='header-container'>
+          <div className='nav-left'>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/polls'>Polls</Link></li>
+            </ul>
+          </div>
+          <div className='nav-right'>
+            <ul>
+              <li><Link to='/admin'>Admin</Link></li>
+              <li><Link to='/login'>Login</Link></li>
+              <li><Link to='/logout'>Logout</Link></li>
+              <li><Link to='/signup'>Signup</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
   </header>
 );
