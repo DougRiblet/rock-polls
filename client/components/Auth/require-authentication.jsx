e2a353fe52dd104e5a0397032537452a2aaa6c85
Doug Riblet <smashzen85@gmail.com>
2018-06-27 23:11:46 -0500

    revert to old require-authentication component, tweak input css

--- client/components/Auth/require-authentication.jsx
+++ client/components/Auth/require-authentication.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 export default function ReqAuth(ComposedComponent) {
-  const Authentication = () => {
-    if (!this.props.authenticated) {
-      return <Redirect to='/' />;
+  class Authentication extends Component {
+    render() {
+      if (!this.props.authenticated) {
+        return <Redirect to='/' />;
+      }
+      return <ComposedComponent {...this.props} />;
     }
-    return <ComposedComponent {...this.props} />;
-  };
+  }
 
   const mapStateToProps = state => ({
     authenticated: state.auth.authenticated,
