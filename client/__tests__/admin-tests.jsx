ea67a1d1b3608888c1ff0911c3cf3b2bdd33734f
Doug Riblet <smashzen85@gmail.com>
2018-06-29 18:09:23 -0500

    add test file for Admin component

---
+++ client/__tests__/admin-tests.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Admin from '../components/Admin/admin-presentational';
+
+jest.mock('react-router-dom', () => ({ Link: 'Link' }))
+
+describe('Admin', () => {
+  test('Snapshot', () => {
+    const tree = renderer.create(<Admin
+      grabMyPolls={() => jest.fn()}
+      myPolls={{}}
+      user_id=''
+      username=''
+    />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Admin
+      grabMyPolls={() => jest.fn()}
+      myPolls={{}}
+      user_id=''
+      username=''
+    />, div);
+  });
+});
