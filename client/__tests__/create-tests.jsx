7e3a7e77d878da2df6b8330dcef1642424f1a671
Doug Riblet <smashzen85@gmail.com>
2018-06-29 22:55:17 -0500

    add test file for Create component

---
+++ client/__tests__/create-tests.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Create from '../components/Create/create-presentational';
+
+describe('Create', () => {
+  test('Snapshot', () => {
+    const tree = renderer.create(<Create
+      createNewPoll={() => jest.fn()}
+      user_id=''
+      username=''
+    />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Create
+      createNewPoll={() => jest.fn()}
+      user_id=''
+      username=''
+    />, div);
+  });
+});
