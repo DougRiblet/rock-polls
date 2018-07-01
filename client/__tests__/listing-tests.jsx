b1e45cbcf76e491773a82da96593279a154f53fa
Doug Riblet <smashzen85@gmail.com>
2018-06-30 19:30:58 -0500

    create test files for Listing component

---
+++ client/__tests__/listing-tests.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Listing from '../components/Listing/listing-presentational';
+
+jest.mock('react-router-dom', () => ({ Link: 'Link' }))
+
+describe('Listing', () => {
+  test('Snapshot', () => {
+    const tree = renderer.create(<Listing
+      grabAllPolls={() => jest.fn()}
+      allPolls={{}}
+    />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Listing
+      grabAllPolls={() => jest.fn()}
+      allPolls={{}}
+    />, div);
+  });
+});
